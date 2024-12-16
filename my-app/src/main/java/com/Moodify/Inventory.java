0e89349608fbbe010c0ab2705effcb2a3877b0c8
Arda Selek <arda.selek@hotmail.com>
2024-12-17 02:17:06 +0300

    top10

--- my-app/src/main/java/com/Moodify/Inventory.java
+++ my-app/src/main/java/com/Moodify/Inventory.java
@@ -26,4 +26,27 @@ public class Inventory {
         }
         return artisttrends;
     }
+
+    //top 10 trend song
+    public static ArrayList<song> findArtistTrendSongs() {
+
+                ArrayList<song> tenTrendSongs = new ArrayList<song>();
+                int size = allSongs.size();
+                while(size != 0) {
+                    size--;
+                    for(int i = 0; i < allSongs.size(); i++) {
+        
+                        if(allSongs.get(i).getPopularity() > allSongs.get(i + 1).getPopularity()) {
+                            song clone = allSongs.get(i + 1);
+                            allSongs.set(i + 1, allSongs.get(i));
+                            allSongs.set(i, clone);    
+                        }
+                    }
+                }
+                //size-10
+                for(int i = allSongs.size(); i > allSongs.size() - 11; i--) {
+                    tenTrendSongs.add(allSongs.get(i));
+                }
+        
+                return tenTrendSongs;
 }
