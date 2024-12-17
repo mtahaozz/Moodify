608780f2b71b6084b10827a55d9a5a7d8bf804da
Taha Özdoğan <ozdogantaha697@gmail.com>
2024-12-17 11:40:57 +0300

    Artistpopularadded

--- my-app/src/main/java/com/Moodify/artist.java
+++ my-app/src/main/java/com/Moodify/artist.java
@@ -3,10 +3,9 @@ package com.Moodify;
 import java.util.ArrayList;
 
 
-
 public class artist {
 
-    private ArrayList<String> popularSongsIDS;
+    private ArrayList<song> popularSongs;
     private String ARTISTNAME;
     private int monthlyListener;
     private String ARTISTID;
@@ -16,13 +15,44 @@ public class artist {
         this.ARTISTNAME = ARTISTNAME;
     }
     
-    public ArrayList<String> getPopularSongIDS(){
-        return popularSongsIDS;
+    public ArrayList<song> getPopularSongs(){
+        return popularSongs;
     }
 
     public void fillArtistIdandSongs() throws Exception{
         ARTISTID = SpotifyAuthHandler.getArtistId(accessToken, ARTISTNAME);
-        popularSongsIDS = SpotifyAuthHandler.getArtistTopTrackIds(accessToken, ARTISTID, "TR");
+    }
+
+    public void fillPopularSongs(){
+        ArrayList<song> allSongs = Inventory.allSongs;
+        ArrayList<song> tenTrendSongs = new ArrayList<song>();
+        ArrayList<song> artistSongs = new ArrayList<>();
+
+        for (song song : allSongs) {
+            if(song.getSongArtist().getARTISTNAME().equals(this.ARTISTNAME)){
+                artistSongs.add(song);
+            }
+        }
+
+        int size = artistSongs.size();
+        
+        while(size != 0) {
+            size--;
+            for(int i = 0; i < artistSongs.size(); i++) {
+
+                if(artistSongs.get(i).getPopularity() > artistSongs.get(i + 1).getPopularity()) {
+                    song clone = artistSongs.get(i + 1);
+                    artistSongs.set(i + 1, artistSongs.get(i));
+                    artistSongs.set(i, clone);    
+                }
+            }
+        }
+        //size-10
+        for(int i = artistSongs.size(); i > artistSongs.size() - 11; i--) {
+            tenTrendSongs.add(artistSongs.get(i));
+        }
+
+        popularSongs = tenTrendSongs;
     }
 
     public String getARTISTNAME() {
